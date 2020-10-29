e5451c213d66922cc08b8e81ac28d618f7d163a7
Tayou <tayou@gmx.net>
2020-10-29 17:05:16 +0100

    main menu and more trying to fix multiplayer

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject ui;
+    
+    private NetworkManager ntwkMgr;
+    // Start is called before the first frame update
+    void Start() {
+        ui.transform.Find("");
+        ntwkMgr.networkAddress = "";
+    }
+
+    public void Connect() {
+    }
+
+    /// <summary>
+    /// Quit App method for button call
+    /// </summary>
+    public void Quit() {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
